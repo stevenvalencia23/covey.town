5f3d3ada5ba6b5a800a6c8565e098b52f29f4c39
selinyuksel <59738776+selinyuksel@users.noreply.github.com>
2021-04-05 18:10:07 -0400

    Create CharacterSettings.tsx
    Started on in-game avatar settings. Completely unfinished!

---
+++ frontend/src/components/Login/CharacterSettings.tsx
@@ -0,0 +1,113 @@
+import React, { useCallback, useState } from 'react';
+
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  useToast
+} from '@chakra-ui/react';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import useCoveyAppState from '../../hooks/useCoveyAppState';
+import useMaybeVideo from '../../hooks/useMaybeVideo';
+import Player, { Character } from '../../classes/Player';
+
+const CharacterSettings: React.FunctionComponent = () => {
+  const {isOpen, onOpen, onClose} = useDisclosure()
+  const video = useMaybeVideo()
+  
+  //TODO: might not need some of this
+  const {apiClient, currentTownID, userName, myPlayerID} = useCoveyAppState();
+
+  const [currentPlayer, setCurrentPlayer] = useState<string>(userName); //TODO this might be all wrong
+  const [characterUpdateAvatar, setCharacterUpdateAvatar] = useState<Character>();
+
+  const openSettings = useCallback(()=>{
+    onOpen();
+    video?.pauseGame();
+  }, [onOpen, video]);
+
+  const closeSettings = useCallback(()=>{
+    onClose();
+    video?.unPauseGame();
+  }, [onClose, video]);
+
+  const toast = useToast()
+  const processUpdates = async (action: string) =>{
+    try {
+        await apiClient. ///update the character
+        updateTown({
+          coveyTownID: currentTownID,
+          coveyTownPassword: roomUpdatePassword,
+          friendlyName,
+          isPubliclyListed
+        });
+        toast({
+          title: 'Character Avatar updated',
+          description: '',
+          status: 'success'
+        })
+        closeSettings();
+      }catch(err){
+        toast({
+          title: 'Unable to update character avatar',
+          description: err.toString(),
+          status: 'error'
+        });
+      }
+    }
+  };
+
+  return <>
+    <MenuItem data-testid='openMenuButton' onClick={openSettings}>
+      <Typography variant="body1">Character Settings</Typography>
+    </MenuItem>
+    <Modal isOpen={isOpen} onClose={closeSettings}>
+      <ModalOverlay/>
+      <ModalContent>
+        <ModalHeader>Edit Avatar of {username} ({myPlayerID})</ModalHeader>
+        <ModalCloseButton/>
+        <form onSubmit={(ev)=>{ev.preventDefault(); processUpdates('edit')}}>
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel htmlFor='friendlyName'>Friendly Name</FormLabel>
+              <Input id='friendlyName' placeholder="Friendly Name" name="friendlyName" value={friendlyName} onChange={(ev)=>setFriendlyName(ev.target.value)} />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel htmlFor='isPubliclyListed'>Publicly Listed</FormLabel>
+              <Checkbox id="isPubliclyListed" name="isPubliclyListed"  isChecked={isPubliclyListed} onChange={(e)=>setIsPubliclyListed(e.target.checked)} />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel htmlFor="updatePassword">Town Update Password</FormLabel>
+              <Input data-testid="updatePassword" id="updatePassword" placeholder="Password" name="password" type="password" value={roomUpdatePassword} onChange={(e)=>setRoomUpdatePassword(e.target.value)} />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button data-testid='deletebutton' colorScheme="red" mr={3} value="delete" name='action1' onClick={()=>processUpdates('delete')}>
+              Delete
+            </Button>
+            <Button data-testid='updatebutton' colorScheme="blue" mr={3} value="update" name='action2' onClick={()=>processUpdates('edit')}>
+              Update
+            </Button>
+            <Button onClick={closeSettings}>Cancel</Button>
+          </ModalFooter>
+        </form>
+      </ModalContent>
+    </Modal>
+  </>
+}
+
+
+export default CharacterSettings;
